f8f0e82571ed17a1587fa8e5ffee99c6725d9775
joheeyong <wh2dyd@naver.com>
2024-10-16 15:26:07 +0900

    Reverpod 마이그레이션 작업 (1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttergetxexam/routes/app_pages.dart';
 import 'package:fluttergetxexam/routes/app_routes.dart';
 import 'package:fluttergetxexam/themes/app_theme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:sembast/sembast.dart';
+import 'data/DatabaseConfig.dart';
+import 'data/LocalStorage.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  LocalStorage();
+  await DatabaseConfig.init(StoreRef<dynamic, dynamic>.main());
   runApp(const MyApp());
 }
 
